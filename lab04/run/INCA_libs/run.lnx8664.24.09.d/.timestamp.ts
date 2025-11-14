9421dfb864bd275bc06c18fa5894890f688f99cf
ksawosz <ksawosz@student.agh.edu.pl>
2025-11-14 02:33:04 +0100

    lab04part2 working

--- lab04/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1760080842 /home/student/ksawosz/VDIC/common/simple_uart_switch.sv
-1763069392 /home/student/ksawosz/VDIC/lab04/tb/top.sv
-1763069720 /home/student/ksawosz/VDIC/lab04/tb/switch_tb_pkg.svh
-1762447249 /home/student/ksawosz/VDIC/lab04/tb/switch_bfm.sv
--- lab04/tb/tb_classes/scoreboard.svh
+++ lab04/tb/tb_classes/scoreboard.svh
@@ -1,5 +1,5 @@
 class scoreboard;
-    
+
     //------------------------------------------------------------------------------
     // local typdefs
     //------------------------------------------------------------------------------
@@ -7,7 +7,7 @@ class scoreboard;
         TEST_PASSED,
         TEST_FAILED
     } test_result;
-    
+
     protected typedef enum {
         COLOR_BOLD_BLACK_ON_GREEN,
         COLOR_BOLD_BLACK_ON_RED,
@@ -16,25 +16,25 @@ class scoreboard;
         COLOR_BLUE_ON_WHITE,
         COLOR_DEFAULT
     } print_color;
-    
+
     protected typedef struct packed{
         logic [7:0] addr;
         bit port;
     } addr_map_t;
-    
+
     //`define DEBUG
-    
+
     //------------------------------------------------------------------------------
     // local variables
     //------------------------------------------------------------------------------
-    
+
     local virtual switch_bfm bfm;
     local test_result tr = TEST_PASSED; // the result of the current test
-    
+
     local addr_map_t prog_table[$];
     pkt_t pkt;
     pkt_t data_queue[$];
-    bit packet_end;
+    logic packet_end;
     logic [21:0] data_out;
 
     //------------------------------------------------------------------------------
@@ -43,7 +43,7 @@ class scoreboard;
     function new (virtual switch_bfm b);
         bfm = b;
     endfunction : new
-    
+
     //------------------------------------------------------------------------------
     // local tasks
     //------------------------------------------------------------------------------
@@ -55,11 +55,14 @@ class scoreboard;
         parity = 0;
         data_out[21:0] = 0;
         bfm.err_packet = 0;
-    
+
         forever begin
             @(negedge bfm.sin);
+            newpkt.is_prog = bfm.prog;
             bfm.err_packet = 0;
-    
+            data_out[21:0] = 0;
+            bfm.err_packet = 0;
+
             for(int k = 0; k < 2; k++) begin
                 parity = 0;
                 repeat (8) @(posedge bfm.clk);
@@ -67,50 +70,6 @@ class scoreboard;
                 $display("1 err = ", bfm.err_packet);
                 `endif
                 if(bfm.sin == 0) begin
-                    if(!k) begin
-                        data_out[0] = bfm.sin;
-                    end
-                    else begin
-                        data_out[11] = bfm.sin;
-                    end
-                end
-                else begin
-                    bfm.err_packet = 1;
-                end
-                `ifdef DEBUG
-                $display("2 err = ", bfm.err_packet);
-                `endif
-    
-                for(i = 0; i < 8; i++) begin
-                    repeat (16) @(posedge bfm.clk);
-                    if(!k) begin
-                        data_out[i+1] = bfm.sin;
-                        parity ^= data_out[i+1];
-                    end
-                    else begin
-                        data_out[i+12] = bfm.sin;
-                        parity ^= data_out[i+12];
-                    end
-                end
-    
-                repeat (16) @(posedge bfm.clk);
-                if(bfm.sin == parity) begin
-                    if(!k) begin
-                        data_out[9] = bfm.sin;
-                    end
-                    else begin
-                        data_out[20] = bfm.sin;
-                    end
-                end
-                else begin
-                    bfm.err_packet = 1;
-                end
-                `ifdef DEBUG
-                $display("3 err = ", bfm.err_packet);
-                `endif
-    
-                repeat (16) @(posedge bfm.clk);
-                if(bfm.sin == 1) begin
                     if(!k) begin
                         data_out[10] = bfm.sin;
                     end
@@ -122,87 +81,134 @@ class scoreboard;
                     bfm.err_packet = 1;
                 end
                 `ifdef DEBUG
+                $display("2 err = ", bfm.err_packet);
+                `endif
+
+                for(i = 9; i > 1; i--) begin
+                    repeat (16) @(posedge bfm.clk);
+                    if(!k) begin
+                        data_out[i] = bfm.sin;
+                        parity ^= data_out[i];
+                    end
+                    else begin
+                        data_out[i+11] = bfm.sin;
+                        parity ^= data_out[i+11];
+                    end
+                end
+
+                repeat (16) @(posedge bfm.clk);
+                if(bfm.sin == parity) begin
+                    if(!k) begin
+                        data_out[1] = bfm.sin;
+                    end
+                    else begin
+                        data_out[12] = bfm.sin;
+                    end
+                end
+                else begin
+                    bfm.err_packet = 1;
+                end
+                `ifdef DEBUG
+                $display("3 err = ", bfm.err_packet);
+                `endif
+
+                repeat (16) @(posedge bfm.clk);
+                if(bfm.sin == 1) begin
+                    if(!k) begin
+                        data_out[0] = bfm.sin;
+                    end
+                    else begin
+                        data_out[11] = bfm.sin;
+                    end
+                end
+                else begin
+                    bfm.err_packet = 1;
+                end
+                `ifdef DEBUG
                 $display("4 err = ", bfm.err_packet);
                 `endif
-    
+
                 repeat (8) @(posedge bfm.clk);
             end
-        
-                newpkt.addr    = data_out[8:1];     
-                newpkt.port    = data_out[12];       
-                newpkt.is_prog = bfm.prog;               
-                newpkt.is_err  = bfm.err_packet;
-                data_queue.push_back(newpkt);             
-    
+
+            newpkt.addr    = data_out[9:2];
+            newpkt.port    = data_out[20];
+            newpkt.is_err  = bfm.err_packet;
+            data_queue.push_back(newpkt);
+
+
             packet_end = 1;
         end
     endtask
-    
+
     local task compare_output();
         bit expected_port;
 
-    forever begin
-        wait (packet_end == 1);
-        packet_end = 0;
+        forever begin
+            wait (packet_end == 1);
+            packet_end = 0;
 
-        for (int i = 0; i < data_queue.size(); i++) begin
-            pkt_t pkt = data_queue[i];
+            for (int i = 0; i < data_queue.size(); i++) begin
+                pkt_t pkt = data_queue[i];
 
-            //------------------------------------------------------
-            // WRONG PACKET CHECKING
-            //------------------------------------------------------
-            if (pkt.is_err) begin
-                $display("[SB] ERR: DUT received wrong packet: addr=%0h port=%0b",
-                         pkt.addr, pkt.port);
-                if(bfm.sout0 == 0 || bfm.sout1 == 1) begin
-                    tr = TEST_FAILED;
-                    $display("[SB] ERR: DUT");
-                end
-                continue;
-            end
-
-            //------------------------------------------------------
-            // PROGRAMMING PACKET
-            //------------------------------------------------------
-            if (pkt.is_prog) begin
-                int idx[$] = prog_table.find_index with (item.addr == pkt.addr);
-
-                if (idx.size() == 0) begin
-                    addr_map_t entry = '{pkt.addr, pkt.port};
-                    prog_table.push_back(entry);
-                    $display("[SB] Added new mapping: addr=%0h -> port=%0b",
-                             pkt.addr, pkt.port);
-                end else begin
-                    prog_table[idx[0]].port = pkt.port;
-                    $display("[SB] Updated mapping: addr=%0h -> port=%0b",
-                             pkt.addr, pkt.port);
-                end
-            end
-
-            //------------------------------------------------------
-            // FUNCTIONAL PACKET (lookup)
-            //------------------------------------------------------
-            else begin
-                int idx[$] = prog_table.find_index with (item.addr == pkt.addr);
-
-                if (idx.size() == 0) begin
-                    $display("[SB] ERR: Unknown address %0h (not programmed!)", pkt.addr);
+                //------------------------------------------------------
+                // WRONG PACKET CHECKING
+                //------------------------------------------------------
+                if (pkt.is_err) begin
+                    `ifdef DEBUG
+                    $display("[SB] ERR: DUT received wrong packet: addr=%0h port=%0b",
+                        pkt.addr, pkt.port);
+                    `endif
+                    if(bfm.sout0 == 0 || bfm.sout1 == 0) begin
+                        tr = TEST_FAILED;
+                    end
                     continue;
                 end
 
-                expected_port = prog_table[idx[0]].port;
+                //------------------------------------------------------
+                // PROGRAMMING PACKET
+                //------------------------------------------------------
+                if (pkt.is_prog) begin
+                    int idx[$] = prog_table.find_index with (item.addr == pkt.addr);
 
-                if (expected_port !== pkt.port) begin
-                    $display("[SB] ERR: Port mismatch for addr %0h: expected=%0b got=%0b",
-                             pkt.addr, expected_port, pkt.port);
-                    tr = TEST_FAILED;
+                    if (idx.size() == 0) begin
+                        addr_map_t entry = '{pkt.addr, pkt.port};
+                        prog_table.push_back(entry);
+                        `ifdef DEBUG
+                        $display("[SB] Added new mapping: addr=%0h -> port=%0b",
+                            pkt.addr, pkt.port);
+                        `endif
+                    end else begin
+                        prog_table[idx[0]].port = pkt.port;
+                        `ifdef DEBUG
+                        $display("[SB] Updated mapping: addr=%0h -> port=%0b",
+                            pkt.addr, pkt.port);
+                        `endif
+                    end
+                end
+                else begin
+                    int idx[$] = prog_table.find_index with (item.addr == pkt.addr);
+
+                    if (idx.size() == 0) begin
+                        $display("[SB] prog=0 ERR: Unknown address %0h (not programmed!)", pkt.addr);
+                        continue;
+                    end
+
+                    expected_port = prog_table[idx[0]].port;
+
+                    if (expected_port !== pkt.port) begin
+                        `ifdef DEBUG
+                        $display("[SB] ERR: Port mismatch for addr %0h: expected=%0b got=%0b",
+                            pkt.addr, expected_port, pkt.port);
+                        `endif
+                        tr = TEST_FAILED;
+                    end
                 end
             end
-        end
 
-        // clear queue after processing
-        data_queue.delete();
-    end
+            // clear queue after processing
+            data_queue.delete();
+        end
     endtask
 
     task execute();
@@ -211,11 +217,11 @@ class scoreboard;
             compare_output();
         join_none
     endtask
-    
+
     //------------------------------------------------------------------------------
     // used to modify the color printed on the terminal
     //------------------------------------------------------------------------------
-    
+
     local function void set_print_color ( print_color c );
         string ctl;
         case(c)
@@ -232,7 +238,7 @@ class scoreboard;
         endcase
         $write(ctl);
     endfunction
-    
+
     //------------------------------------------------------------------------------
     // print the PASSED/FAILED in color
     //------------------------------------------------------------------------------
@@ -254,13 +260,12 @@ class scoreboard;
             $write ("\n");
         end
     endfunction
-    
+
     //------------------------------------------------------------------------------
     // print the test result at the simulation end
     //------------------------------------------------------------------------------
     function void print_result();
         print_test_result(tr);
     endfunction
-    
+
 endclass : scoreboard
-    
